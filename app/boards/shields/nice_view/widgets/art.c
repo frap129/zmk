478e5ed83674d50ed21d3900731c44a40d013cb7
Joe Maples <joe@maples.dev>
2026-03-03 11:41:17 -0500

    fix(art): Remove reserved header

--- app/boards/shields/nice_view/widgets/art.c
+++ app/boards/shields/nice_view/widgets/art.c
@@ -326,7 +326,6 @@ const LV_ATTRIBUTE_MEM_ALIGN LV_ATTRIBUTE_LARGE_CONST LV_ATTRIBUTE_IMG_BRANDOFSA
 
 const lv_img_dsc_t BrandOfSacrifice = {
     .header.cf = LV_COLOR_FORMAT_I1,
-    .header.reserved = 0,
     .header.w = 140,
     .header.h = 68,
     .data_size = 1232,
