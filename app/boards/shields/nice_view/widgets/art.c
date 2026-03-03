b8b66d576e8e085e9f731c2d94c686c7901ac874
Joe Maples <joe@maples.dev>
2026-03-03 11:38:27 -0500

    fix(art): Remove always zero header

--- app/boards/shields/nice_view/widgets/art.c
+++ app/boards/shields/nice_view/widgets/art.c
@@ -326,7 +326,6 @@ const LV_ATTRIBUTE_MEM_ALIGN LV_ATTRIBUTE_LARGE_CONST LV_ATTRIBUTE_IMG_BRANDOFSA
 
 const lv_img_dsc_t BrandOfSacrifice = {
     .header.cf = LV_COLOR_FORMAT_I1,
-    .header.always_zero = 0,
     .header.reserved = 0,
     .header.w = 140,
     .header.h = 68,
